12b122015836fa8517433199fd52026c5c987549
Gordana Minovska <ekogoca@gmail.com>
2017-04-24 12:45:04 -0700

    Add route /headers to see the request headers

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ var randomFortune =
 fortunes[Math.floor(Math.random() * fortunes.length)];
 res.render('about', { fortune: randomFortune });
 });
+//chapter 6 request header route
+app.get('/headers', function(req,res){
+res.set('Content-Type','text/plain');
+var s = '';
+for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+res.send(s);
+});
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
 res.status(404);
@@ -40,6 +47,7 @@ console.error(err.stack);
 res.status(500);
 res.render('500');
 });
+
 
 app.listen(app.get('port'), function(){
 console.log( 'Express started on http://localhost:' +
